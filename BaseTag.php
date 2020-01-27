f6de5a144193ee8a80feda0e0ed45b1107c3f0a0
phptheme <dev@getphptheme.com>
2020-01-27 15:55:59 +0300

    n/a

---
+++ BaseTag.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @author PhpTheme Dev Team <dev@getphptheme.com>
+ * @license MIT
+ * @link http://getphptheme.com
+ */
+namespace PhpTheme\Tag;
+
+use PhpTheme\HtmlHelper\HtmlHelper;
+
+abstract class BaseTag
+{
+
+    public $tag;
+
+    public $attributes = [];
+
+    public $renderEmpty = true;
+
+    public $content;
+
+    public function __construct(array $params = [])
+    {
+        foreach($params as $key => $value)
+        {
+            $this->$key = $value;
+        }
+    }
+
+    public static function factory(array $params = [])
+    {
+        $class = static::class;
+
+        $return = new $class($params);
+
+        return $return;
+    }
+
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    public function toString() : string
+    {
+        $content = $this->getContent();
+
+        if (!$content && !$this->renderEmpty)
+        {
+            return '';
+        }
+
+        return HtmlHelper::tag($this->tag, $content, $this->attributes);        
+    }
+
+    public function __toString()
+    {
+        return $this->toString();
+    }
+
+}
---
+++ Tag.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author PhpTheme Dev Team <dev@getphptheme.com>
+ * @license MIT
+ * @link http://getphptheme.com
+ */
+namespace PhpTheme\Tag;
+
+class Tag extends BaseTag
+{
+}
